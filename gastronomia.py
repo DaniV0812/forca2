f6c0cf4fbd6846bc32cb7aa9bdd6c926f2aaaa51
DaniV0812 <daniele.campos.santos@escola.pr.gov.br>
2026-05-06 19:47:50 -0300

    Add files via upload

---
+++ gastronomia.py
@@ -0,0 +1,13 @@
+
+gastronomia = [
+        "pizza", "hamburguer", "cachorro quente", "lasanha", "macarrao",
+        "feijoada", "churrasco", "arroz e feijao", "strogonoff", "frango grelhado",
+        "sushi", "temaki", "yakisoba", "ramen", "sashimi",
+        "taco", "burrito", "nachos", "guacamole", "quesadilla",
+        "pastel", "coxinha", "kibe", "esfiha", "empada",
+        "bolo de chocolate", "brigadeiro", "beijinho", "pudim", "sorvete",
+        "panqueca", "waffle", "crepe", "croissant", "donut",
+        "salada", "omelete", "tapioca", "açai", "granola",
+        "pao de queijo", "cuscuz", "farofa", "moqueca", "acaraje",
+        "batata frita", "nuggets", "hot roll", "poke", "hamburguer artesanal"
+    ]
